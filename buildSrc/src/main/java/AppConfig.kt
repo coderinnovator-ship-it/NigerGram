1f5121ba7ce70f74507ebc58549054fabee414ad
coderinnovator-ship-it <coderinnovator@gmail.com>
2026-05-30 14:06:29 +0100

    Update AppConfig.kt

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -1,5 +1,6 @@
 /**
- * Created by Puskal Khadka on 3/14/2023.
+ * Created in Zetra Lab on 05/30/2026.
+ * Developed by Zetra Company.
  */
 object AppConfig {
     const val minSdk = 21
@@ -7,5 +8,5 @@ object AppConfig {
     const val compileSdk = 33
     const val versionCode = 1
     const val VersionName = "1.0.0"
-    const val KotlinCompilerExtension="1.4.2"
+    const val KotlinCompilerExtension = "1.4.2"
 }
