68b099289038cc2defa814255bc780883c2be0ce
coderinnovator-ship-it <coderinnovator@gmail.com>
2026-05-30 17:05:04 +0100

    Update build.gradle.kts

--- common/composable/build.gradle.kts
+++ common/composable/build.gradle.kts
@@ -1,13 +1,6 @@
-plugins {
-    id("plugin.android-common")
-}
-
 dependencies {
-    // These link the bridge to your other modules
-    implementation(project(":common:theme"))
-    implementation(project(":domain"))
-    implementation(project(":data"))
-    implementation(project(":core"))
-
+    api(project(":core"))
+    api(project(":data"))
+    api(project(":common:theme"))
     media3Dependency()
 }
