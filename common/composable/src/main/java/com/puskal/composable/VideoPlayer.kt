bdec05614676df5b2287ec0b65a57c5081e53f33
coderinnovator-ship-it <coderinnovator@gmail.com>
2026-05-30 16:40:26 +0100

    Update VideoPlayer.kt

--- common/composable/src/main/java/com/puskal/composable/VideoPlayer.kt
+++ common/composable/src/main/java/com/puskal/composable/VideoPlayer.kt
@@ -1,146 +1,79 @@
 package com.puskal.composable
 
-
-import android.graphics.Bitmap
-import android.net.Uri
-import android.view.ViewGroup
-import androidx.compose.animation.*
-import androidx.compose.animation.core.*
-import androidx.compose.foundation.*
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.pager.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.pager.PagerState
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
-import androidx.media3.common.C
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.ui.AspectRatioFrameLayout
 import androidx.media3.ui.PlayerView
-import coil.compose.AsyncImage
 import com.puskal.core.utils.FileUtils
+import com.puskal.core.utils.IntentUtils
 import com.puskal.data.model.VideoModel
-import com.puskal.theme.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-/**
- * Created by Puskal Khadka on 3/16/2023.
- */
-@OptIn(ExperimentalFoundationApi::class)
-@androidx.annotation.OptIn(androidx.media3.common.util.UnstableApi::class)
 @Composable
 fun VideoPlayer(
     video: VideoModel,
     pagerState: PagerState,
     pageIndex: Int,
-    onSingleTap: (exoPlayer: ExoPlayer) -> Unit,
-    onDoubleTap: (exoPlayer: ExoPlayer, offset: Offset) -> Unit,
-    onVideoDispose: () -> Unit = {},
-    onVideoGoBackground: () -> Unit = {}
+    onSingleTap: (ExoPlayer) -> Unit,
+    onDoubleTap: (ExoPlayer, androidx.compose.ui.geometry.Offset) -> Unit,
+    onVideoDispose: () -> Unit,
+    onVideoGoBackground: () -> Unit
 ) {
     val context = LocalContext.current
-    var thumbnail by remember {
-        mutableStateOf<Pair<Bitmap?, Boolean>>(Pair(null, true))  //bitmap, isShow
+    val exoPlayer = remember {
+        ExoPlayer.Builder(context).build().apply {
+            repeatMode = Player.REPEAT_MODE_ONE
+            playWhenReady = true
+        }
     }
-    var isFirstFrameLoad = remember { false }
 
-    LaunchedEffect(key1 = true) {
-        withContext(Dispatchers.IO) {
-            val bm = FileUtils.extractThumbnail(
-                context.assets.openFd("videos/${video.videoLink}"), 1
+    DisposableEffect(video.videoUrl) {
+        val mediaItem = MediaItem.fromUri(video.videoUrl)
+        exoPlayer.setMediaItem(mediaItem)
+        exoPlayer.prepare()
+        onDispose {
+            onVideoDispose()
+            exoPlayer.release()
+        }
+    }
+
+    LaunchedEffect(pagerState.currentPage) {
+        if (pagerState.currentPage == pageIndex) {
+            exoPlayer.playWhenReady = true
+        } else {
+            exoPlayer.playWhenReady = false
+        }
+    }
+
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .pointerInput(Unit) {
+            detectTapGestures(
+                onTap = { onSingleTap(exoPlayer) },
+                onDoubleTap = { offset -> onDoubleTap(exoPlayer, offset) }
             )
-            withContext(Dispatchers.Main) {
-                thumbnail = thumbnail.copy(first = bm, second = thumbnail.second)
-            }
         }
-    }
-    if (pagerState.settledPage == pageIndex) {
-        val exoPlayer = remember(context) {
-            ExoPlayer.Builder(context).build().apply {
-                videoScalingMode = C.VIDEO_SCALING_MODE_SCALE_TO_FIT
-                repeatMode = Player.REPEAT_MODE_ONE
-                setMediaItem(MediaItem.fromUri(Uri.parse("asset:///videos/${video.videoLink}")))
-                playWhenReady = true
-                prepare()
-                addListener(object : Player.Listener {
-                    override fun onRenderedFirstFrame() {
-                        super.onRenderedFirstFrame()
-                        isFirstFrameLoad = true
-                        thumbnail = thumbnail.copy(second = false)
-                    }
-                })
-            }
-        }
-
-        val lifecycleOwner by rememberUpdatedState(LocalLifecycleOwner.current)
-        DisposableEffect(key1 = lifecycleOwner) {
-            val lifeCycleObserver = LifecycleEventObserver { _, event ->
-                when (event) {
-                    Lifecycle.Event.ON_STOP -> {
-                        exoPlayer.pause()
-                        onVideoGoBackground()
-                    }
-                    Lifecycle.Event.ON_START -> exoPlayer.play()
-                    else -> {}
+    ) {
+        AndroidView(
+            factory = { ctx ->
+                PlayerView(ctx).apply {
+                    player = exoPlayer
+                    useController = false
+                    resizeMode = androidx.media3.ui.AspectRatioFrameLayout.RESIZE_MODE_ZOOM
                 }
-            }
-            lifecycleOwner.lifecycle.addObserver(lifeCycleObserver)
-            onDispose {
-                lifecycleOwner.lifecycle.removeObserver(lifeCycleObserver)
-            }
-        }
-
-        val playerView = remember {
-            PlayerView(context).apply {
-                player = exoPlayer
-                useController = false
-                resizeMode = AspectRatioFrameLayout.RESIZE_MODE_ZOOM
-                layoutParams = ViewGroup.LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT
-                )
-            }
-        }
-
-        DisposableEffect(key1 = AndroidView(factory = {
-            playerView
-        }, modifier = Modifier.pointerInput(Unit) {
-            detectTapGestures(onTap = {
-                onSingleTap(exoPlayer)
-            }, onDoubleTap = { offset ->
-                onDoubleTap(exoPlayer, offset)
-            })
-        }), effect = {
-            onDispose {
-                thumbnail = thumbnail.copy(second = true)
-                exoPlayer.release()
-                onVideoDispose()
-            }
-        })
-    }
-
-    if (thumbnail.second) {
-        AsyncImage(
-            model = thumbnail.first,
-            contentDescription = null,
-            modifier = Modifier.fillMaxSize(),
-            contentScale = ContentScale.Crop
+            },
+            modifier = Modifier.fillMaxSize()
         )
     }
-
 }
-
-
-
-
-
