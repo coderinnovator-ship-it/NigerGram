b308af217bd23a74794c386dbb03366053afbae8
coderinnovator-ship-it <coderinnovator@gmail.com>
2026-05-30 12:56:03 +0100

    Update MyApp.kt

--- app/src/main/java/com/puskal/tiktokcompose/MyApp.kt
+++ app/src/main/java/com/puskal/tiktokcompose/MyApp.kt
@@ -3,6 +3,11 @@ package com.nigergram.app
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+/**
+ * Created in Zetra Lab on 05/30/2026.
+ * Developed by Zetra Company.
+ */
+
 @HiltAndroidApp
 class MyApp : Application() {
     override fun onCreate() {
