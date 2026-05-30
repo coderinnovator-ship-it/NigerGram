98a8861fc33b7923dd4d33710b35da70677d8cae
coderinnovator-ship-it <coderinnovator@gmail.com>
2026-05-30 12:29:12 +0100

    Update MyApp.kt

--- app/src/main/java/com/puskal/tiktokcompose/MyApp.kt
+++ app/src/main/java/com/puskal/tiktokcompose/MyApp.kt
@@ -1,11 +1,8 @@
-package com.puskal.tiktokcompose
+package com.nigergram.app
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
-/**
- * Created by Puskal Khadka on 3/14/2023.
- */
 @HiltAndroidApp
 class MyApp : Application() {
     override fun onCreate() {
